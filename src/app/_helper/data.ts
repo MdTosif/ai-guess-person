18e27cb352c6230ef78ce7886d72d603118c89f3
mdtosif <md.tosif@zeeve.io>
2024-10-01 00:10:34 +0530

    some fixes and added question

--- src/app/_helper/data.ts
+++ src/app/_helper/data.ts
@@ -1,4 +1,83 @@
 export const apiData = [
-  { q: "choose one", a: ["white", "pink"] },
-  { q: "choose one", a: ["car", "cycle"] },
+  {
+    q: "Choose an environment you prefer:",
+    a: ["City", "Countryside", "Ocean", "Mountains"],
+  },
+  {
+    q: "Which type of character do you relate to more?",
+    a: ["Strategist", "Brave", "Caring", "Mysterious"],
+  },
+  {
+    q: "Choose a color that resonates with you:",
+    a: ["Blue", "Red", "Green", "Black"],
+  },
+  {
+    q: "What kind of relationship do you find most interesting?",
+    a: ["Friendship", "Rivalry", "Romance", "Family bond"],
+  },
+  {
+    q: "Pick a leisure activity:",
+    a: ["Reading books", "Exploring outdoors", "Playing games", "Meditating"],
+  },
+  {
+    q: "Which type of narrative pace do you prefer?",
+    a: [
+      "Slow and thoughtful",
+      "Fast and action-packed",
+      "Balanced",
+      "Unpredictable",
+    ],
+  },
+  {
+    q: "Choose a form of transport:",
+    a: ["Train", "Motorbike", "Flying ship", "Walking"],
+  },
+  {
+    q: "Pick a type of superpower:",
+    a: [
+      "Mind control",
+      "Super strength",
+      "Elemental control",
+      "Time manipulation",
+    ],
+  },
+  {
+    q: "Choose a favorite weather condition:",
+    a: ["Sunny", "Rainy", "Snowy", "Windy"],
+  },
+  {
+    q: "Pick a mythical creature to befriend:",
+    a: ["Dragon", "Fairy", "Phoenix", "Giant wolf"],
+  },
+  {
+    q: "If you were part of a team, what role would you prefer?",
+    a: ["Leader", "Strategist", "Healer", "Scout"],
+  },
+  {
+    q: "Which would you rather fight for?",
+    a: ["Justice", "Freedom", "Peace", "Honor"],
+  },
+  {
+    q: "Which element do you feel connected to?",
+    a: ["Fire", "Water", "Earth", "Air"],
+  },
+  {
+    q: "Which personality trait do you value most?",
+    a: ["Courage", "Wisdom", "Loyalty", "Kindness"],
+  },
+  {
+    q: "Pick a time of day you feel most energized:",
+    a: ["Morning", "Afternoon", "Evening", "Night"],
+  },
+  {
+    q: "If you had a partner in crime, who would they be?",
+    a: [
+      "A childhood friend",
+      "A mysterious stranger",
+      "A talking animal",
+      "A robotic companion",
+    ],
+  },
+  { q: "Pick a creature to battle:", a: ["Demon", "Alien", "Robot", "Ghost"] },
+  { q: "Pick a favorite season:", a: ["Spring", "Summer", "Autumn", "Winter"] },
 ];
